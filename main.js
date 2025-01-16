232fac075d53cc30ca5075e49c2a1ba891b20709
matias <you@example.com>
2025-01-16 13:15:51 -0300

    Chage path import

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ import copyToClipBoard from "./js/copyToClipboard.js";
 import getAndInsertQuote from "./js/getAndInsertQuote.js";
 import getRandomQuote from "./js/randomQuote.js";
 
-getAndInsertQuote()
-getRandomQuote()
-copyToClipBoard()
+getAndInsertQuote();
+getRandomQuote();
+copyToClipBoard();
